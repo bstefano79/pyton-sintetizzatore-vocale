f2d73c19632fa53bac4ea424224723c0e0481b64
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2022-06-10 12:33:03 +0200

    prima interazzione con richiesta nome

---
+++ assitente_vocale.py
@@ -0,0 +1,20 @@
+from pyttsx3 import init
+import speech_recognition as sr
+
+engine = init()
+engine.say("ciao ciao! come ti chiami?")
+engine.runAndWait()
+
+r=sr.Recognizer()
+parole = None
+with sr.Microphone() as source:
+    print("Pronto ad ascoltare...")
+    try:
+        audio = r.listen(source)
+        parole = r.recognize_google(audio, language="it-IT")
+    except sr.UnknownValueError:
+        print("non hai detto niente")
+    
+    
+if(parole is not None):
+    print(parole)
